62dcadf8ddb3363c043400b40a01bfdef8e11f74
Hyun Sung Cho <hcho774@gmail.com>
2022-04-05 09:59:40 -0400

    commit tree 51f0cd025d71b72d51f2ef25ed65c0fd7db19fb6

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr) {
+    return (arr.slice(0,2));
+}
+
+const returnLastTwoDrivers = function(arr) {
+    return (arr.slice(-2));
+}
+
+console.log(returnLastTwoDrivers(["amy","cho","david"]));
+
+const selectingDrivers = [
+    returnFirstTwoDrivers, returnLastTwoDrivers
+];
+
+function createFareMultiplier(int) {
+    return function(fare) {
+        return int * fare;
+    }
+}
+
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (arrayOfDrivers, func) {
+    if (func === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(arrayOfDrivers);
+    } else if (func === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(arrayOfDrivers);
+    }
+}
+
